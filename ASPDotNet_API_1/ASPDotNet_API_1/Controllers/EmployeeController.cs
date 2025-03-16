a0cf6fe2324083f1ed4393dc01c45aa979442f43
MohamedEl-Tabei <mohamedeltabei2000@gmail.com>
2025-03-16 20:14:29 +0200

    ASP .NET API

---
+++ ASPDotNet_API_1/ASPDotNet_API_1/Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using ASPDotNet_API_1.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNet_API_1.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class EmployeeController
+    {
+        private readonly static List<Employee> employees = Employee.GetEmployees();
+
+
+        #region Get
+        [HttpGet]
+        public Ok<List<Employee>> Get()
+        {
+            return TypedResults.Ok(employees);
+        }
+        #endregion
+
+        #region Get By Id
+        [HttpGet("{id}", Name = nameof(GetById))]
+        public Results<NotFound<string>, Ok<Employee>> GetById(Guid id)
+        {
+            var employee = employees.FirstOrDefault(x => x.Id == id);
+            if (employee is null) return TypedResults.NotFound("Invalid Id");
+            return TypedResults.Ok(employee);
+
+        }
+        #endregion
+
+        #region Create 
+        [HttpPost]
+        public CreatedAtRoute<Employee> Create(Employee newEmployee)
+        {
+            newEmployee.Id = Guid.NewGuid();
+            employees.Add(newEmployee);
+            return TypedResults.CreatedAtRoute(newEmployee, nameof(GetById), new { id = newEmployee.Id });
+        }
+        #endregion
+
+        #region Update
+        [HttpPut("{id}")]
+        public Results<NoContent, NotFound, BadRequest> Update(Guid id, Employee newData)
+        {
+            if (newData.Id != id) return TypedResults.BadRequest();
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+            if (employee is null) return TypedResults.NotFound();
+
+            employee.Name = newData.Name;
+            employee.Salary = newData.Salary;
+            return TypedResults.NoContent();
+
+        }
+        #endregion
+
+        #region Delete
+        [HttpDelete("{id}")]
+        public Results<NotFound, NoContent> Delete(Guid id)
+        {
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+
+            if (employee is null) return TypedResults.NotFound();
+
+            employees.Remove(employee);
+            return TypedResults.NoContent();
+        }
+        #endregion
+    }
+}
---
+++ ASPDotNet_API_1/ASPDotNet_API_1/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace ASPDotNet_API_1.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Salary { get; set; }
+        public static List<Employee> GetEmployees()=>new List<Employee>()
+        {
+            new Employee(){Id=Guid.NewGuid(),Name="Ali",Salary=5000},
+            new Employee(){Id=Guid.NewGuid(),Name="Ahmed",Salary=7000},
+            new Employee(){Id=Guid.NewGuid(),Name="Basem",Salary=8000},
+            new Employee(){Id=Guid.NewGuid(),Name="Aya",Salary=4000},
+            new Employee(){Id=Guid.NewGuid(),Name="Omer",Salary=6000},
+            new Employee(){Id=Guid.NewGuid(),Name="Amr",Salary=4000},
+        };
+    }
+}
